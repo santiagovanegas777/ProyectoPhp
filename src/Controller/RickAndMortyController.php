e98974a976e0f1c51c10d6c966c502353d82b335
santiagovanegas777 <correavanegassantiago@gmail.com>
2023-07-16 20:22:21 +0200

    features

--- src/Controller/RickAndMortyController.php
+++ src/Controller/RickAndMortyController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Character;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Response;
 
 class RickAndMortyController extends AbstractController{
 
@@ -24,41 +29,50 @@ class RickAndMortyController extends AbstractController{
         return $this->render("Characters/characters.html.twig",["character"=>$character]);
     }
     #[Route("/rickandmortylist")]
-    public function listCharacters(){
+    public function listCharacters(EntityManagerInterface $doctrineS){
+        $repository = $doctrineS->getRepository(Character::class);
+        $characters = $repository->findAll();
 
-        $characters = [
-            [
-                "name"=>"Rick",
-                "species"=>"Alien",
-                "image"=>"https://rickandmortyapi.com/api/character/avatar/14.jpeg",
-                "gender"=>"Male",
-    
-    
-    
-            ],
-            [
-                "name"=>"Million Ants",
-                "species"=>"Animal",
-                "image"=>"https://rickandmortyapi.com/api/character/avatar/226.jpeg",
-                "gender"=>"Male",
-    
-    
-    
-            ],
-            [
-                "name"=>"Ricktiminus Sancheziminius",
-                "species"=>"Human",
-                "image"=>"https://rickandmortyapi.com/api/character/avatar/294.jpeg",
-                "gender"=>"Male",
-    
-    
-    
-            ],
 
-        ];
+        
         return $this->render("Characters/listcharacters.html.twig",["characters"=>$characters]);
 
     }
 
+    #[Route("new/character")]
+    public function newCharacter(EntityManagerInterface $doctrineS)
+    {
+        $character1 =  new Character ();
+        $character1->setName("Rick");
+        $character1->setSpecies("Alien");
+        $character1->setImage("https://rickandmortyapi.com/api/character/avatar/14.jpeg");
+        $character1->setGender("Male");
+
+        $character2 =  new Character ();
+        $character2->setName("Million Ants");
+        $character2->setSpecies("Animal");
+        $character2->setImage("https://rickandmortyapi.com/api/character/avatar/226.jpeg");
+        $character2->setGender("Male");
+
+        $character3 =  new Character ();
+        $character3->setName("Ricktiminus Sancheziminius");
+        $character3->setSpecies("Human");
+        $character3->setImage("https://rickandmortyapi.com/api/character/avatar/294.jpeg");
+        $character3->setGender("Male");
+
+
+        $doctrineS->persist($character1);
+        $doctrineS->persist($character2);
+        $doctrineS->persist($character3);
+
+        $doctrineS->flush();
+        return new Response("characters insertados correctamente");
+
+    
+
+
+
+        // return new Response("Estoy en la home")
+    }
 
 }
---
+++ src/Entity/Character.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+#[ORM\Table(name: '`character`')]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $species = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $gender = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSpecies(): ?string
+    {
+        return $this->species;
+    }
+
+    public function setSpecies(?string $species): static
+    {
+        $this->species = $species;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+}
