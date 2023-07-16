77f846747c029b2d068ea51982f71bfb7183fe97
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2023-07-16 15:23:29 +0200

    routes

--- src/Controller/RickAndMortyController.php
+++ src/Controller/RickAndMortyController.php
@@ -23,6 +23,42 @@ class RickAndMortyController extends AbstractController{
 
         return $this->render("Characters/characters.html.twig",["character"=>$character]);
     }
+    #[Route("/rickandmortylist")]
+    public function listCharacters(){
+
+        $characters = [
+            [
+                "name"=>"Rick",
+                "species"=>"Alien",
+                "image"=>"https://rickandmortyapi.com/api/character/avatar/14.jpeg",
+                "gender"=>"Male",
+    
+    
+    
+            ],
+            [
+                "name"=>"Million Ants",
+                "species"=>"Animal",
+                "image"=>"https://rickandmortyapi.com/api/character/avatar/226.jpeg",
+                "gender"=>"Male",
+    
+    
+    
+            ],
+            [
+                "name"=>"Ricktiminus Sancheziminius",
+                "species"=>"Human",
+                "image"=>"https://rickandmortyapi.com/api/character/avatar/294.jpeg",
+                "gender"=>"Male",
+    
+    
+    
+            ],
+
+        ];
+        return $this->render("Characters/listcharacters.html.twig",["characters"=>$characters]);
+
+    }
 
 
 }
