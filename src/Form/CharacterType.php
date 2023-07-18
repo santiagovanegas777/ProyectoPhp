7db37687da69ce75530e652380a15d1d2ba6f5d4
santiagovanegas777 <correavanegassantiago@gmail.com>
2023-07-18 14:46:29 +0200

    formulario relacion entre tablas config

--- src/Form/CharacterType.php
+++ src/Form/CharacterType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Character;
+use App\Entity\Episode;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,11 +15,25 @@ class CharacterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('species')
-            ->add('image')
-            ->add('gender')
-            // ->add('episodes')
+            ->add('name', null,[
+                'label'=>'NAME',
+                'attr' => ['placeholder'=>'Pon aqui el nombre del character']
+
+            ])
+            ->add('species',null,[
+                'attr' => ['placeholder'=>'Pon aqui la especie']
+            ])
+            ->add('image',null,[
+                'attr' => ['placeholder'=>'Carga una imagen']
+            ])
+            ->add('gender',null,[
+                'attr' => ['placeholder'=>'Pon aqui el gender del character']
+            ])
+             ->add('episodes', EntityType::class,[
+                'class' =>Episode::class,
+                'choice_label'=> 'episode',
+                'multiple'=>true,
+             ])
             ->add('Enviar', SubmitType::class);
         ;
     }
